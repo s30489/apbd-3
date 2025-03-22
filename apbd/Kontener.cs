0bc4078cdf73718db7e215189dced6f502d4db41
Adam Wyszynski <s30489@pjwstk.edu.pl>
2025-03-23 00:40:05 +0100

    "added Kontener class"

---
+++ apbd/Kontener.cs
@@ -0,0 +1,23 @@
+﻿namespace apbd;
+
+public class Kontener
+{
+    private double masa;
+    double wysokosc;
+    double wagaKontenera;
+    private double glebokosc;
+    private string nrSeryjny = "KON-";
+    private double maxLadownosc;
+
+    static int nrKont = 1;
+
+    public void oproznienie()
+    {
+
+    }
+
+    public void zaladowanie()
+    {
+
+    }
+}
